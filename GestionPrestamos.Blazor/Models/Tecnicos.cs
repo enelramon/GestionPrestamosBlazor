b5a77d3c323ed6c87c28155a75f43d84823788b4
Enel Almonte <enel_almonte@ucne.edu.do>
2025-01-17 18:37:58 -0400

    Add Bootstrap script and improve layout in PrestamoIndex; introduce Tecnicos model

---
+++ GestionPrestamos.Blazor/Models/Tecnicos.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GestionPrestamos.Models;
+
+public class Tecnicos
+{
+    [Key]
+    public int TecnicoId { get; set; }
+
+    [Required(ErrorMessage = "Este campo es requerido")]
+    public string Nombres { get; set; } = null!;
+
+    [Range(1, double.MaxValue, ErrorMessage = "El sueldo no puede ser menor a 1")]
+    public double SueldoHora { get; set; }
+}

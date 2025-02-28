59afe4b8c97e9a606cb327944ebe684cc496e732
Adrian Hernández Bonilla <adrian_hernandez@ucne.edu.do>
2025-02-28 19:47:15 -0400

    Proyecto Modelo Creado para la Api

---
+++ ModelosPrestamo/Prestamos.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace ModelosPrestamo;
+
+public partial class Prestamos
+{
+    [Key]
+    public int PrestamoId { get; set; } // No es requerido porque es autoincremental
+
+    [Required(ErrorMessage = "El campo Concepto es obligatorio.")]
+    public string Concepto { get; set; } = null!;
+
+    [Required(ErrorMessage = "El campo Monto es obligatorio.")]
+    [Range(0.01, double.MaxValue, ErrorMessage = "El Monto debe ser mayor a 0.")]
+    public double Monto { get; set; }
+
+   
+    public double Balance { get; set; }
+
+    [Required(ErrorMessage = "El campo DeudorId es obligatorio.")]
+    [Range(1, int.MaxValue, ErrorMessage = "Debe seleccionar un deudor válido.")]
+    public int DeudorId { get; set; }
+
+    [ForeignKey("DeudorId")]
+    [InverseProperty("Prestamos")]
+    public virtual Deudores Deudor { get; set; } = null!;
+
+    [InverseProperty("Prestamo")]
+    public virtual ICollection<PrestamosDetalle> PrestamosDetalle { get; set; } = new List<PrestamosDetalle>();
+
+}

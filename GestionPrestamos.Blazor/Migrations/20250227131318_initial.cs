f769861fb2e9bec7d289a2beb7a2b09f882e1150
Adrian Hernández Bonilla <adrian_hernandez@ucne.edu.do>
2025-02-28 19:51:32 -0400

    Se Creo la migracion para la api

---
+++ GestionPrestamos.Blazor/Migrations/20250227131318_initial.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GestionPrestamos.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Balance",
+                table: "PrestamosDetalles",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Balance",
+                table: "PrestamosDetalles");
+        }
+    }
+}
